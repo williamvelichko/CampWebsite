9bbf06c49c4e3f6c678f205c72c991de919c2dc6
williamvelichko <williamvelichko2003@gmail.com>
2024-06-17 10:53:39 -0700

    dynamic

--- src/app/components/Video/Video.tsx
+++ src/app/components/Video/Video.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/Video.module.scss";
 
 export const Video = () => {
+  const [videoSrc, setVideoSrc] = useState({
+    webm: "/camp_invite_vertical.webm",
+    mp4: "/camp_invite_vertical.mp4",
+  });
+
+  useEffect(() => {
+    const updateVideoSource = () => {
+      if (window.innerWidth < 600) {
+        setVideoSrc({
+          webm: "/camp_invite_vertical.webm",
+          mp4: "/camp_invite_vertical.mp4",
+        });
+      } else {
+        setVideoSrc({
+          webm: "/camp_invite_horizontal.webm",
+          mp4: "/camp_invite_horizontal.mp4",
+        });
+      }
+    };
+
+    updateVideoSource();
+    window.addEventListener("resize", updateVideoSource);
+
+    return () => window.removeEventListener("resize", updateVideoSource);
+  }, []);
   return (
     <section className={styles.videoSection}>
       <video
+        key={videoSrc.mp4}
         controls
         autoPlay
         loop
@@ -12,8 +39,8 @@ export const Video = () => {
         preload="auto"
         className={styles.video}
       >
-        <source src="/camp_invite_4.webm" type="video/webm" />
-        <source src="/camp_invite_4.mp4" type="video/mp4" />
+        <source src={videoSrc.webm} type="video/webm" />
+        <source src={videoSrc.mp4} type="video/mp4" />
         <track
           src="/path/to/captions.vtt"
           kind="subtitles"
