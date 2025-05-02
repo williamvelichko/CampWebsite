1079e720d9b1217a627de14ea7e070e7b5a28c8f
williamvelichko <williamvelichko2003@gmail.com>
2025-05-01 17:16:34 -0700

    save imgs

--- src/app/components/MainSection/MainSection.tsx
+++ src/app/components/MainSection/MainSection.tsx
@@ -1,14 +1,64 @@
-import React from "react";
+// import React from "react";
+// import styles from "./styling/MainSection.module.scss";
+// import Image from "next/image";
+// import Link from "next/link";
+
+// export const MainSection = () => {
+//   return (
+//     <div className={styles.hero}>
+//       <div className={styles.imageWrapper}>
+//         <Image
+//           src="/GloryOfGodVert.jpg"
+//           alt="Camp Adventure"
+//           layout="fill"
+//           className={styles.image}
+//           objectFit="cover"
+//           loading="eager"
+//         />
+//       </div>
+//       <div className={styles.overlay}>
+//         <div className={styles.textContainer}>
+//           <Link href="/registration" className={styles.registerButton}>
+//             Register
+//           </Link>
+
+//           <div className={styles.bounceArrow}>
+//             <span>&#8595;</span>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./styling/MainSection.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
 export const MainSection = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 800);
+    };
+
+    handleResize(); // Set initial value
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const imageSrc = isMobile
+    ? "/GloryOfGodVert.jpg"
+    : "/GloryOfGodHorizontal.jpg";
+
   return (
     <div className={styles.hero}>
       <div className={styles.imageWrapper}>
         <Image
-          src="/GloryOfGod.jpg"
+          src={imageSrc}
           alt="Camp Adventure"
           layout="fill"
           className={styles.image}
@@ -21,7 +71,6 @@ export const MainSection = () => {
           <Link href="/registration" className={styles.registerButton}>
             Register
           </Link>
-
           <div className={styles.bounceArrow}>
             <span>&#8595;</span>
           </div>
