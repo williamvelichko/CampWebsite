f15ba7226403ff3abdcfed2c33db1d5bf6389e43
williamvelichko <williamvelichko2003@gmail.com>
2025-05-01 17:46:31 -0700

    styling change

--- src/app/components/MainSection/MainSection.tsx
+++ src/app/components/MainSection/MainSection.tsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import styles from "./styling/MainSection.module.scss";
-// import Image from "next/image";
-// import Link from "next/link";
-
-// export const MainSection = () => {
-//   return (
-//     <div className={styles.hero}>
-//       <div className={styles.imageWrapper}>
-//         <Image
-//           src="/GloryOfGodVert.jpg"
-//           alt="Camp Adventure"
-//           layout="fill"
-//           className={styles.image}
-//           objectFit="cover"
-//           loading="eager"
-//         />
-//       </div>
-//       <div className={styles.overlay}>
-//         <div className={styles.textContainer}>
-//           <Link href="/registration" className={styles.registerButton}>
-//             Register
-//           </Link>
-
-//           <div className={styles.bounceArrow}>
-//             <span>&#8595;</span>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 "use client";
 
 import React, { useEffect, useState } from "react";
